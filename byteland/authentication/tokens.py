d34a0014b418b9b469070a47540d773c5b64f466
mshirdel <mshirdel@gmail.com>
2021-03-19 23:59:35 +0330

    update requirements and fix six package

--- byteland/authentication/tokens.py
+++ byteland/authentication/tokens.py
@@ -1,13 +1,12 @@
 from django.contrib.auth.tokens import PasswordResetTokenGenerator
-from django.utils import six
 
 
 class UserEmailActivationTokenGenerator(PasswordResetTokenGenerator):
     def _make_hash_value(self, user, timestamp):
         return (
-            six.text_type(user.pk) +
-            six.text_type(timestamp) +
-            six.text_type(user.email_confirmed)
+            str(user.pk) +
+            str(timestamp) +
+            str(user.email_confirmed)
         )
 
 user_email_activation_token = UserEmailActivationTokenGenerator()
